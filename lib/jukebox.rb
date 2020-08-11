03cc0df0c0fb994f6a24c5d8f3ea21948843ba7c
Gabriel Hicks <gabrielhicks@mail.com>
2020-08-11 17:31:26 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -41,6 +41,7 @@ def run(songs)
   user_input = gets.strip
   
   case user_input
+  
   when "exit"
     exit_jukebox
   when "help"
@@ -50,6 +51,5 @@ def run(songs)
   when "play"
     play(songs)
   end
-  
   
 end
