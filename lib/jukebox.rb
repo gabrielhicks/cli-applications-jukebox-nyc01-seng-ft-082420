80f01b80366e2c674b73ce27544c401a0f126764
Gabriel Hicks <gabrielhicks@mail.com>
2020-08-11 17:22:23 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -19,7 +19,8 @@ def play(songs)
   index = user_input.to_i - 1
 
   if songs[index] == nil || songs.none?(user_input)
-    puts "Invalid"
+    puts "Invalid input, please try again"
+  end
   
   if index >= 0 && user_input.length == 1
     puts "Playing #{songs[index]}"
